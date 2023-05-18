48fa904859fb9c3e576fcacd666771584ad02f6f
Oberdan Angelim <oberdan.dev@gmail.com>
2023-05-18 00:16:10 -0300

    Create a pdf template

--- server/index.js
+++ server/index.js
@@ -8,7 +8,36 @@ app.use(express.json());
 app.use(cors());
 app.listen(3000, ()=> console.log('listening on http://localhost:3000'));
 
-const pdf = await createPDF();
+const invoice = {
+	shipping: {
+		name: 'John Doe',
+		address: '1234 Main Street',
+		city: 'San Francisco',
+		state: 'CA',
+		country: 'US',
+		postal_code: 94111,
+	},
+	items: [
+		{
+			item: 'TC 100',
+			description: 'Toner Cartridge',
+			quantity: 2,
+			amount: 6000,
+		},
+		{
+			item: 'USB_EXT',
+			description: 'USB Cable Extender',
+			quantity: 1,
+			amount: 2000,
+		},
+	],
+	subtotal: 8000,
+	paid: 0,
+	invoice_nr: 1234,
+};
+
+
+const pdf = await createPDF(invoice, './src/docs/teste.pdf');
 
 app.use('/download', async(req,res)=> {
   res.sendFile(path.resolve(pdf));
--- server/pdf.js
+++ server/pdf.js
@@ -1,90 +1,57 @@
-import PDFDocument from 'pdfkit'
 import PDFTable from 'pdfkit-table';
+import pkg from 'pdfkit';
 import fs from 'fs'
+import path from 'path';
+import PDFDocument from 'pdfkit'
+
 const doc = new PDFDocument();
 
-const createPDF = async () => {
-  const date = Date.now()
-  const clientName = 'Joao'
+function generateHeader(doc) {
+	doc.image(path.resolve('./assets/img/logotipo.png'), 100, 45, { width: 150 })
+		.fillColor('#444444')
+		.fontSize(20)
+		.fontSize(10)
+		.text('Rua Frei Pantaleao de Aveiro', 200, 75, { align: 'right' })
+		.text('N24 - Manaus, Amazonas', 200, 90, { align: 'right' })
+		.moveDown();
+}
 
-  const pathThisPDF = `./output/invoice_${clientName}_${date}.pdf`
+function generateFooter(doc) {
+	doc.fontSize(
+		10,
+	).text(
+		'Payment is due within 15 days. Thank you for your business.',
+		50,
+		780,
+		{ align: 'center', width: 500 },
+	);
+}
 
-  
-  const primaryColor = '#008080';
-  const secondaryColor = '#d9d9d9';
-  
-  // Adicione a tabela de informações
-  const table = {
-    headers: ['Aluno', 'Endereço', 'Professor', 'Endereço', 'Data', 'Valor'],
-    rows: [
-      ['Fulano de Tal', 'Rua Tal, 123', 'Beltrano de Tal', 'Rua XYZ, 456', '15/05/2023', 'R$ 50,00']
-    ]
-  };
-  
-  // Configure o layout da tabela
-  const tableLayout = {
-    fillColor: secondaryColor,
-    textColor: primaryColor,
-    fontSize: 10,
-    padding: 2
-  };
-  
-  // Defina a largura das colunas
-  const columnWidths = {
-    0: 100,
-    1: 150,
-    2: 100,
-    3: 150,
-    4: 80,
-    5: 80
-  };
-  
-  // Crie uma nova tabela e adicione ao documento
-  const pdfTable = new PDFTable(doc, {
-    bottomMargin: 30
-  });
-  pdfTable.addPlugin(new (require('pdfkit-tables/plugins/fitcolumn'))({
-    columnWidths: columnWidths
-  }));
-  pdfTable.addPlugin(new (require('pdfkit-tables/plugins/autoTableAlt'))());
-  pdfTable.on('pageAdded', function () {
-    pdfTable.table.x = pdfTable.page.margins.left;
-    pdfTable.table.y = pdfTable.page.margins.top;
-  });
-  pdfTable.on('cellAfterAdded', function (cell, data) {
-    if (cell.y === 0) {
-      cell.styles.fillColor = primaryColor;
-      cell.styles.color = '#fff';
-    } else if (cell.y % 2 === 0) {
-      cell.styles.fillColor = secondaryColor;
-    }
-  });
-  pdfTable.draw(table, {
-    x: 50,
-    y: 50,
-    prepareHeader: () => doc.font('Helvetica-Bold'),
-    prepareRow: () => doc.font('Helvetica'),
-    layout: tableLayout
-  });
-  
-  doc.fontSize(14).text('Nome do aluno:', { continued: true });
-  doc.fontSize(12).text(' Fulano de Tal');
-  doc.fontSize(14).text('Endereço:', { continued: true });
-  doc.fontSize(12).text(' Rua Tal, 123');
-  doc.moveDown();
-  doc.fontSize(14).text('Nome do professor:', { continued: true });
-  doc.fontSize(12).text(' Beltrano de Tal');
-  doc.fontSize(14).text('Endereço:', { continued: true });
-  doc.fontSize(12).text(' Rua XYZ, 456');
-  doc.moveDown();
-  doc.fontSize(14).text('Valor da aula:', { continued: true });
-  doc.fontSize(12).text(' R$ 50,00');
-  doc.fontSize(14).text('Data:', { continued: true });
-  doc.fontSize(12).text(' 15/05/2023');
-  doc.pipe(fs.createWriteStream(pathThisPDF));
-  doc.end();
+function generateCustomerInformation(doc, invoice){
+  const shipping = invoice.shipping;
+	doc.text(`Invoice Number: ${invoice.invoice_nr}`, 50, 210)
+		.text(`Invoice Date: ${new Date()}`, 50, 215)
+		.text(`Balance Due: ${invoice.subtotal - invoice.paid}`, 50, 130)
+		.text(shipping.name, 300, 500)
+		.text(shipping.address, 300, 500)
+		.text(
+			`${shipping.city}, ${shipping.state}, ${shipping.country}`,
+			300,
+			130,
+		)
+		.moveDown();
+}
 
-  return pathThisPDF;
+const createPDF = async (invoice, path) => {
+  let doc = new PDFDocument({ margin: 50 });
+
+  generateCustomerInformation(doc, invoice)
+
+	generateHeader(doc); // Invoke `generateHeader` function.
+	generateFooter(doc); // Invoke `generateFooter` function.
+
+	doc.end();
+	doc.pipe(fs.createWriteStream(path));
 }
 
 export default createPDF;
